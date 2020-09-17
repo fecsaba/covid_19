e4e0d25066bb7400f1769b7f586c0cff8142a4b7
fecsaba <nexusegyesulet@gmail.com>
2020-09-17 17:38:28 +0200

    16. rész

--- src/components/MainPage/MainPage.vue
+++ src/components/MainPage/MainPage.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="MainPage">
     <Header title="Covid Statistics" />
+    <Table />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Table from "@/components/MainPage/Table";
 export default {
-  components: { Header },
+  components: { Table, Header },
   props: {}
 };
 </script>
---
+++ src/components/MainPage/Table.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Table</div>
+</template>
+
+<script>
+import * as axios from "axios";
+
+export default {
+  name: "Table",
+  mounted() {
+    axios
+      .get("https://api.thevirustracker.com/free-api?countryTotals=ALL")
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
+
+<style scoped></style>
