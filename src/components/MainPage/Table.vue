e5b961c7d22738cbb4e7cc4c04215fbe20a6efc9
fecsaba <nexusegyesulet@gmail.com>
2020-09-21 16:58:16 +0200

    17. rész

--- src/components/MainPage/Table.vue
+++ src/components/MainPage/Table.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>Table</div>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -11,11 +42,17 @@ export default {
     axios
       .get("https://api.thevirustracker.com/free-api?countryTotals=ALL")
       .then(res => {
-        console.log(res);
+        console.log(Object.values(res.data.countryitems[0]));
+        this.countries = Object.values(res.data.countryitems[0]);
       })
       .catch(err => {
         console.log(err);
       });
+  },
+  data() {
+    return {
+      countries: null
+    };
   }
 };
 </script>
