292c6aaf15c1091007e6c1fed9923fe2d564219c
fecsaba <nexusegyesulet@gmail.com>
2020-10-12 12:22:08 +0200

    22-24. rész

--- src/components/Details/Card.vue
+++ src/components/Details/Card.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="card text-center">
     <div class="card-header">
-      Featured
+      <h3 class="display-4">
+        {{ date }}
+      </h3>
     </div>
     <div class="card-body">
-      <h5 class="card-title">{{ countryName }}</h5>
-      <p class="card-text">
-        With supporting text below as a natural lead-in to additional content.
-      </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div class="mb-3">
+        <h5 class="card-title display-4">{{ countryName }}</h5>
+      </div>
+      <p class="card-text"></p>
+      <Progress :max="maxCases" :daily="dailyCases" :msg="'Daily Cases'" />
+      <Progress :max="maxDeaths" :daily="dailyDeaths" :msg="'Daily Deaths'" />
+      <form>
+        <div class="form-group">
+          <label for="formControlRange">Date</label>
+          <input
+            type="range"
+            class="form-control-range"
+            id="formControlRange"
+            min="0"
+            :max="daysCount"
+            v-model="i"
+          />
+        </div>
+      </form>
     </div>
     <div class="card-footer text-muted">
-      2 days ago
+      <button class="btn btn-primary" @click="goHome()">Go back</button>
     </div>
   </div>
 </template>
 
 <script>
+import Progress from "@/components/Details/Progress";
+import router from "@/router";
 export default {
   name: "Card",
+  components: { Progress },
   props: {
     countryName: { type: String, default: null, required: false },
     detailsObj: { type: Object, default: null, required: false }
@@ -28,11 +47,45 @@ export default {
   },
   methods: {
     anim() {
-      // const days = Object.keys(this.detailsObj).length - 2;
+      console.log("mounted");
+      this.i = 0;
+      this.daysCount = Object.keys(this.detailsObj).length - 2;
+      const days = Object.keys(this.detailsObj);
       const valueArr = Object.values(this.detailsObj);
       valueArr.pop();
-      console.log(valueArr);
+      days.pop();
+      const dailyCases = valueArr.map(o => o.new_daily_cases);
+      const dailyDeaths = valueArr.map(o => o.new_daily_deaths);
+      this.maxCases = Math.max(...dailyCases);
+      this.maxDeaths = Math.max(...dailyDeaths);
+      // console.log(dailyCases);
+      const step = () => {
+        this.date = days[this.i];
+        this.dailyCases = valueArr[this.i].new_daily_cases;
+        this.dailyDeaths = valueArr[this.i].new_daily_deaths;
+        // console.log(days[i]);
+        if (this.i === this.daysCount) {
+          clearInterval(si);
+        }
+        this.i++;
+      };
+
+      let si = setInterval(step, 1000);
+    },
+    goHome() {
+      router.back()
     }
+  },
+  data() {
+    return {
+      date: null,
+      maxCases: 0,
+      maxDeaths: 0,
+      dailyCases: 0,
+      dailyDeaths: 0,
+      daysCount: 0,
+      i: 0
+    };
   }
 };
 </script>
---
+++ src/components/Details/Progress.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mb-5 mt-3">
+    <h5>{{ msg }}</h5>
+    <div class="progress">
+      <div
+        class="progress-bar"
+        role="progressbar"
+        :style="{ width: (daily / max) * 100 + '%' }"
+        :aria-valuenow="daily"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        {{ daily }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Progress",
+  props: {
+    daily: { type: Number, default: null, required: false },
+    max: { type: Number, default: null, required: false },
+    msg: { type: String, default: null, required: false }
+  }
+};
+</script>
+
+<style scoped></style>
